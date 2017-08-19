5009510ec007bff049331207c75a8e0794aa7be5
Sujeet kumar <31015598+sujeetkk@users.noreply.github.com>
2017-08-20 02:01:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 //Move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight() {
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft='px';
+}
 img.onclick=function() {
     var interval=setInterval(moveRight,100);
 };
