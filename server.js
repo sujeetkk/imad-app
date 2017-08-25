d2e73c1f725b146139ce66ae5c0aa1b557731525
Sujeet kumar <31015598+sujeetkk@users.noreply.github.com>
2017-08-25 16:57:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', function (req, res) {
 app.get('/article_one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
+
+app.get('/article_two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
+});
+
+app.get('/article_three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article_three.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
