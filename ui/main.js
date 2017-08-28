54459aa5041175e3719c86941dbaa0ec3274879b
Sujeet kumar <31015598+sujeetkk@users.noreply.github.com>
2017-08-29 01:04:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,3 +9,12 @@ function moveRight() {
 img.onclick=function() {
     var interval=setInterval(moveRight,100);
 };
+
+//Counter code
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+};
