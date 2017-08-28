206bd9a431feb8af215fd32581f682fe7411977e
Sujeet kumar <31015598+sujeetkk@users.noreply.github.com>
2017-08-29 01:24:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,24 @@ img.onclick=function() {
 
 //Counter code
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
-    counter=counter+1;
+//create a request
+    var request=new XMLHttpRequest();
+    
+    //capture the response
+    request.onreadystatechange=function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+        if(request.status===200){
+    var counter=request.responseText;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
+        }
+        }
+        //Noe done yet
+    };
+    //make the request
+    request.open('GET','http://sujeetk270396.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
