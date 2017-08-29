21746fab11e0ea66f166d89691b704a7c73a7bc7
Sujeet kumar <31015598+sujeetkk@users.noreply.github.com>
2017-08-30 01:37:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,9 +41,9 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function() {
 //make request
 //capture a list
-var name=['name1','name2','name3','name4'];
+var names=['name1','name2','name3','name4'];
 var list='';
-for(var i=0;i<name.length;i++){
+for(var i=0;i<names.length;i++){
     list+='<li>' +name[i] + '</li>';
 }
 var ul=document.getElementById('namelist');
