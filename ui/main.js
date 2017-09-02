07d6b685fa510f95993e39fc67f25585d7a1a937
Sujeet kumar <31015598+sujeetkk@users.noreply.github.com>
2017-09-02 23:25:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,15 +22,21 @@ button.onclick=function(){
         if(request.readyState === XMLHttpRequest.DONE) {
             //take some action
         if(request.status===200){
-    var counter=request.responseText;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+   //capture a list
+var names request.responsText;
+names=JSON.parse(name);
+var list='';
+for(var i=0;i<names.length;i++){
+    list+='<li>' +names[i] + '</li>';
+}
+var ul=document.getElementById('namelist');
+ul.innerHTML=list;
         }
         }
         //Noe done yet
     };
     //make the request
-    request.open('GET','http://sujeetk270396.imad.hasura-app.io/counter',true);
+    request.open('GET','http://sujeetk270396.imad.hasura-app.io/submit-name?name' +name,true);
     request.send(null);
 };
 
@@ -40,12 +46,5 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function() {
 //make request
-//capture a list
-var names=['name1','name2','name3','name4'];
-var list='';
-for(var i=0;i<names.length;i++){
-    list+='<li>' +names[i] + '</li>';
-}
-var ul=document.getElementById('namelist');
-ul.innerHTML=list;
+
 };
